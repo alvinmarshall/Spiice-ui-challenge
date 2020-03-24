b68e2f19678afcd84997aae9f37e01b69564d666
bik_krlvn <kelvinbirikorang@gmail.com>
2020-03-24 19:43:05 +0000

    initial commit

---
+++ app/src/main/java/com/cheise_proj/spiice_ui_challenge/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cheise_proj.spiice_ui_challenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
